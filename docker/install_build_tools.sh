5321f1d1097828c44e70999e116105cfe286f40f
Haolun <chshawkn@hotmail.com>
2017-07-06 01:32:42 +0800

    upgrade maven and gradle

--- docker/install_build_tools.sh
+++ docker/install_build_tools.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-MAVEN_PACKAGE="apache-maven-3.3.9"
-GRADLE_PACKAGE="gradle-3.3"
+MAVEN_PACKAGE="apache-maven-3.5.0"
+GRADLE_PACKAGE="gradle-3.5"
 
 MAVEN_BALL="${MAVEN_PACKAGE}-bin.tar.gz"
 GRADLE_BALL="${GRADLE_PACKAGE}-bin.zip"
 
 if [ -z "${build_fileserver}" ]; then
     build_fileserver="http://mirrors.tuna.tsinghua.edu.cn";
-    wget -q -O ${MAVEN_BALL} ${build_fileserver}/apache/maven/maven-3/3.3.9/binaries/${MAVEN_BALL}
+    wget -q -O ${MAVEN_BALL} ${build_fileserver}/apache/maven/maven-3/3.5.0/binaries/${MAVEN_BALL}
     build_fileserver="https://downloads.gradle.org";
     wget -q -O ${GRADLE_BALL} ${build_fileserver}/distributions/${GRADLE_BALL}
 else
-    wget -q -O ${MAVEN_BALL} ${build_fileserver}/apache/maven/maven-3/3.3.9/binaries/${MAVEN_BALL}
+    wget -q -O ${MAVEN_BALL} ${build_fileserver}/apache/maven/maven-3/3.5.0/binaries/${MAVEN_BALL}
     wget -q -O ${GRADLE_BALL} ${build_fileserver}/distributions/${GRADLE_BALL}
 fi
 
